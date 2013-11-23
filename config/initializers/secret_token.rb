d1d14fe01f3a77b2f5f8f4ef506cdaa21e293a01
Moises Zaragoza <mZaragoza@circletechfl.com>
2013-11-23 13:45:00 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Easytowticket::Application.config.secret_key_base = '115f099b7daced83f99e519fcf58591c210c11778bb50ccb78f3055b1ac0dee0261fe4172de8e2fffa9c5278a616162409b32a3e304e95c86d4b551990b32476'
