bcc2faf84b0f7953ded0b2c1b2767df3e3f8daea
Moises Zaragoza <mZaragoza@circletechfl.com>
2013-12-27 09:35:12 -0500

    Driver Dashboard

--- app/controllers/admins/dashboards_controller.rb
+++ app/controllers/admins/dashboards_controller.rb
@@ -1,5 +1,11 @@
 class Admins::DashboardsController < AdminController
-
   expose(:admins){ current_account.admins.where(:is_driver => true, :active => true).order("id DESC").scoped{} }
+  expose(:tickets){
+    if current_admin.is_admin
+      current_account.tickets.order("id DESC").scoped{}
+    else
+      current_admin.tickets.order("id DESC").scoped{}
+    end
+  }
 end
 
